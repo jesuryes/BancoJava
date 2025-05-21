8ed58401e57ca5826451443494360114facd7813
jesuryes <161269641+jesuryes@users.noreply.github.com>
2025-05-21 12:33:47 -0600

    listo

--- src/Banco.java
+++ src/Banco.java
@@ -1,39 +1,43 @@
 public class Banco {
     //Atributos - Privados
-     private String titular;
-     private  double saldo;
+    private String titular;
+    private double saldo;
 
-     //Constructor (Método)
+    //Constructor (Método)
     public Banco (String titular, double saldoInicial){
-this.titular = titular;
-this.saldo = saldoInicial;
+        this.titular = titular;
+        this.saldo = saldoInicial;
     }
+
     //Métodos que son comunes en los bancos
-    //Método para obtener dinero
-    public String getTitular (){
-        return this.titular;
+    //Método para obtener el nombre del titular
+    public String getTitular() {
+        return titular;
     }
 
     //Método para obtener el saldo
-    public double getSaldo(){
-        return this.saldo;
+    public double getSaldo() {
+        return saldo;
     }
+
     //Método para depositar dinero
-    public void  depositar (double cantidad) {
-        //Es nuestros  métodos es justo y necesario que hagamos validaciones
-        if (cantidad > 0) {
+    public void depositar(double cantidad){
+        //En nuestros métodos es justo y necesario que hagamos validaciones
+        if(cantidad > 0){
             saldo += cantidad;
-            System.out.printf("Deposito realizado con éxito. Nuevo saldo: $" + saldo);
-        } else {
+            System.out.println("Deposito realizado con éxito. Nuevo saldo: $" + saldo);
+        }else{
             System.out.println("Cantidad no valida para depositar");
         }
     }
+
     //Método para retirar dinero
     public void retirar(double cantidad){
-        if (cantidad > 0 && cantidad <= saldo){
+        if(cantidad > 0 && cantidad <= saldo){
             saldo -= cantidad;
-            System.out.println("Retiro exitoso, Saldo restante $"+ saldo);
+            System.out.println("Retiro exitoso, Saldo restante $" + saldo);
         }else {
-    }
+            System.out.println("Saldo insuficiente para retirar");
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Crear una cuenta nueva
+        Banco cuenta1 = new Banco("Kevin", 1000);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Mostrar el saldo inicial
+        System.out.println("Titular: " + cuenta1.getTitular());
+        System.out.println("Saldo inicial: $" + cuenta1.getSaldo());
+
+        // Realizar un depósito
+        cuenta1.depositar(500);
+
+        // Realizar un retiro
+        cuenta1.retirar(300);
+
+        // Intentar retirar más de lo disponible
+        cuenta1.retirar(2000);
     }
 }
